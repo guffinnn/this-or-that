7c0fee978533af3641e958b06b1076ad3e53dbed
Danik <dashkevich_daniil2006@mail.ru>
2023-12-09 07:53:24 +0300

    completed adaptive for gamepage and starting for routes and functionality

--- src/pages/game/GamePage.jsx
+++ src/pages/game/GamePage.jsx
@@ -7,14 +7,18 @@ function GamePage() {
     return (
         <div className="container">
             <div className="container__fluid">
-                <div className="navigation__frame">
-                    <Button />
-                </div>
+
                 <div className="main__frame">
-                    <div className="heading__frame">
-                        <h1>Голосование</h1>
-                        <p className="text">выбери один из вариантов</p>
+                    <div className="header">
+                        <div className="heading__frame">
+                            <h1>Голосование</h1>
+                            <p className="text">выбери один из вариантов</p>
+                        </div>
+                        <div className="navigation__frame">
+                            <Button />
+                        </div>
                     </div>
+
                     <div className="cards__frame">
                         <CardLeft />
                         <CardRight />
