eb1a8b22cc5d9aa0e0c6c12a1463b4f5a5a79615
Danik <dashkevich_daniil2006@mail.ru>
2023-12-18 17:42:53 +0300

    installed firebase, but it's not working now (???), tried to fixed that

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJOo5a41jAnRznkr9w_baBGtS5VS9jWLI",
+    authDomain: "this-or-that-e2ae5.firebaseapp.com",
+    databaseURL: "https://this-or-that-e2ae5-default-rtdb.firebaseio.com",
+    projectId: "this-or-that-e2ae5",
+    storageBucket: "this-or-that-e2ae5.appspot.com",
+    messagingSenderId: "966143518475",
+    appId: "1:966143518475:web:4920638907b67701dfa40e",
+    measurementId: "G-18D8E2NTQW"
+};
+
+initializeApp(firebaseConfig);
--- src/pages/game/GamePage.jsx
+++ src/pages/game/GamePage.jsx
@@ -1,33 +1,45 @@
+import {Component} from 'react';
 import './GamePage.css';
-import Button from "../../components/button/Button";
-import CardLeft from "../../components/card-left/CardLeft";
-import CardRight from "../../components/card-right/CardRight";
+import Button from '../../components/button/Button';
+import CardLeft from '../../components/card-left/CardLeft';
+import CardRight from '../../components/card-right/CardRight';
 import { Link } from 'react-router-dom';
 
-function GamePage() {
-    return (
-        <div className="container__game">
-            <div className="container__fluid__game">
-                <div className="main__frame__game">
-                    <div className="header">
-                        <div className="heading__frame__game">
-                            <h1 className="head__game">Голосование</h1>
-                            <p className="text">выбери один из вариантов</p>
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import '../../firebase';
+
+class GamePage extends Component {
+    componentDidMount = () => {
+        const database = firebase.database().ref('/cards');
+        console.log(database);
+    };
+
+    render() {
+        return (
+            <div className='container__game'>
+                <div className='container__fluid__game'>
+                    <div className='main__frame__game'>
+                        <div className='header'>
+                            <div className='heading__frame__game'>
+                                <h1 className='head__game'>Голосование</h1>
+                                <p className='text'>выбери один из вариантов</p>
+                            </div>
+                            <div className='navigation__frame__game'>
+                                <Link to={'/this-or-that'}>
+                                    <Button />
+                                </Link>
+                            </div>
                         </div>
-                        <div className="navigation__frame__game">
-                            <Link to={'/this-or-that'}>
-                                <Button />
-                            </Link>
+                        <div className='cards__frame'>
+                            <CardLeft />
+                            <CardRight />
                         </div>
-                    </div>
-                    <div className="cards__frame">
-                        <CardLeft />
-                        <CardRight />
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default GamePage;
