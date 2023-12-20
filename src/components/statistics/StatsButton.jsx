4d795f01e5b3b5b198db445d7fe8163b0fb22c71
Danik <dashkevich_daniil2006@mail.ru>
2023-12-20 18:00:09 +0300

    changed a blocks visuality on mobile, and fixed a bug in safari browser, and changed statistics visuality and data

--- src/components/statistics/StatsButton.jsx
+++ src/components/statistics/StatsButton.jsx
@@ -6,14 +6,18 @@ function StatsButton() {
     return (
         <div className="statistics">
             <div className="stats__button__frame">
-                <img className="svg" alt="Button" src={stats} width="29" />
+                <img className="svg" alt="Button" src={stats} width="24" />
             </div>
             <div className="statistics__frame">
                 <div className="heading__stats">
                     <h3 className="heading">Таблица лидеров</h3>
                 </div>
                 <div className="stats__mainframe">
-                    <LeaderCard />
+                    <LeaderCard index="1" name="Банановый герой" points={10000} />
+                    <LeaderCard index="2" name="Пушистый шалун" points={5700} />
+                    <LeaderCard index="3" name="Кокосовый бешенец" points={4300} />
+                    <LeaderCard index="4" name="Веселая хвостатая" points={2700} />
+                    <LeaderCard index="5" name="Смешной кулик" points={2000} />
                 </div>
             </div>
         </div>
